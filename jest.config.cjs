f343b1e62801f529c47ab8132be79a4d8c88bf1e
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2022-06-18 19:57:32 -0400

    add lint-staged/husky

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
+module.exports = config;
