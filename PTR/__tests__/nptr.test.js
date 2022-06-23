76ee90330bd68d191b8204cc6dcfde9444a9e6ea
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2022-06-23 08:57:26 -0400

    add rig for testing against control samples

--- PTR/__tests__/nptr.test.js
+++ PTR/__tests__/nptr.test.js
@@ -9,24 +9,17 @@ describe('nptr output matches control samples', () => {
       nptr(`PTR/__tests__/TEST_OUTPUT/${controlFile.slice(0, -4)}-TEST`, {
         ...args,
       });
+      const testBuffer = fs.readFileSync(
+        `PTR/__tests__/TEST_OUTPUT/${controlFile.slice(0, -4)}-TEST.gif`,
+      );
+      const controlBuffer = fs.readFileSync(
+        `PTR/__tests__/control_samples/${controlFile.slice(0, -4)}-CS.gif`,
+      );
+      expect(testBuffer.equals(controlBuffer));
+
+      // we should delete the file we just created in TEST_OUTPUT
     });
   }
 });
-
-// We can directly compare output files
-/*
-const file1 = fs.readFileSync("./PTR_output/test.gif")
-const file2 = fs.readFileSync("./PTR_output/test copy.gif")
-file1.equals(file2) // -> true
-
-const file3 = fs.readFileSync("./PTR_output/someOtherFile.gif")
-file1.equals(file2) // -> false
-*/
-
-/*
-In order to do this, we'll need to be able to run the entire program from the test
-Currently the only way to pass params is via the command line
-So we'll need a wrapper function we can call, passing in args
-
-We'll probably have to manually clean up after each test, deleting the test output
- */
+// maybe the cleanup should happen in an afterAll block - let all tests run
+// then clean out the entire dir
